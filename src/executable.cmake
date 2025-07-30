a9fa39b7e1f4bd365ccfe9aabcae26a104555fd3
Chris Vig <chris@invictus.so>
2025-07-29 19:57:41 -0500

    Initial work on supporting assembly projects.

--- src/executable.cmake
+++ src/executable.cmake
@@ -26,9 +26,15 @@ project(${CMAKE_PROJECT_NAME}::${EXECUTABLE_NAME})
 
 # -- Validate Arguments --
 
-# Default to no libraries if not specified otherwise
+# Set default arguments
 if(NOT DEFINED EXECUTABLE_LIBS)
     set(EXECUTABLE_LIBS "")
+endif()
+if(NOT DEFINED EXECUTABLE_COMPILE_OPTIONS)
+    set(EXECUTABLE_COMPILE_OPTIONS "")
+endif()
+if(NOT DEFINED EXECUTABLE_LINK_OPTIONS)
+    set(EXECUTABLE_LINK_OPTIONS "")
 endif()
 
 # Ensure required arguments are set
@@ -41,6 +47,21 @@ endif()
 if(NOT DEFINED EXECUTABLE_LIBS)
     message(FATAL_ERROR "EXECUTABLE_LIBS must be defined!")
 endif()
+if(NOT DEFINED EXECUTABLE_COMPILE_OPTIONS)
+    message(FATAL_ERROR "EXECUTABLE_COMPILE_OPTIONS must be defined!")
+endif()
+if(NOT DEFINED EXECUTABLE_LINK_OPTIONS)
+    message(FATAL_ERROR "EXECUTABLE_LINK_OPTIONS must be defined!")
+endif()
+
+# -- Local Variables --
+
+# Names for supplemental files
+set(EXECUTABLE_DUMP_FILE            ${EXECUTABLE_NAME}.txt)
+set(EXECUTABLE_ELF_FILE             ${EXECUTABLE_NAME}.elf)
+set(EXECUTABLE_ELF_STRIPPED_FILE    ${EXECUTABLE_NAME}.elf.stripped)
+set(EXECUTABLE_IHEX_FILE            ${EXECUTABLE_NAME}.ihex)
+set(EXECUTABLE_MAP_FILE             ${EXECUTABLE_NAME}.map)
 
 # -- Main Executable --
 
@@ -48,6 +69,21 @@ endif()
 add_executable(
     ${EXECUTABLE_NAME}
     ${EXECUTABLE_SOURCE}
+)
+
+# Set compile options
+target_compile_options(
+    ${EXECUTABLE_NAME}
+    PRIVATE
+    ${EXECUTABLE_COMPILE_OPTIONS}
+)
+
+# Set linker options
+target_link_options(
+    ${EXECUTABLE_NAME}
+    PRIVATE
+    ${EXECUTABLE_LINK_OPTIONS}
+    -Wl,-Map=${PROJECT_BUILD_DIR}/${EXECUTABLE_MAP_FILE}
 )
 
 # Add libraries
@@ -60,31 +96,53 @@ target_link_libraries(
 set_target_properties(
     ${EXECUTABLE_NAME}
     PROPERTIES
-        OUTPUT_NAME ${EXECUTABLE_NAME}.elf
+        OUTPUT_NAME ${EXECUTABLE_ELF_FILE}
 )
 
-# -- IHEX Executable --
+# -- Supplemental Files --
 
-# Add command for IHEX output
+# Add commands for supplemental files
 add_custom_command(
-    OUTPUT ${EXECUTABLE_NAME}.ihex
-    DEPENDS ${PROJECT_BIN_DIR}/${EXECUTABLE_NAME}.elf
+    # List outputs of the command
+    OUTPUT ${EXECUTABLE_DUMP_FILE}
+    OUTPUT ${EXECUTABLE_ELF_STRIPPED_FILE}
+    OUTPUT ${EXECUTABLE_IHEX_FILE}
+    # Make this command depend on the main ELF file
+    # (the ${PROJECT_BIN_DIR} prefix is necessary for some reason)
+    DEPENDS ${PROJECT_BIN_DIR}/${EXECUTABLE_ELF_FILE}
+    # Step 1 - Generate the dump file using avr-objdump
+    COMMAND ${AVR_OBJDUMP}
+    ARGS -x -D -S ${EXECUTABLE_ELF_FILE} > ${EXECUTABLE_DUMP_FILE}
+    # Step 2 - Create a copy of the main ELF file to strip
+    COMMAND cp
+    ARGS ${EXECUTABLE_ELF_FILE} ${EXECUTABLE_ELF_STRIPPED_FILE}
+    # Step 3 - Strip the copy with avr-strip
     COMMAND ${AVR_STRIP}
-    ARGS ${EXECUTABLE_NAME}.elf
+    ARGS ${EXECUTABLE_ELF_STRIPPED_FILE}
+    # Step 4 - Convert the stripped ELF file to IHEX, removing any EEPROM section
     COMMAND ${AVR_OBJCOPY}
-    ARGS -R .eeprom -O ihex ${EXECUTABLE_NAME}.elf ${EXECUTABLE_NAME}.ihex
+    ARGS -R .eeprom -O ihex ${EXECUTABLE_ELF_STRIPPED_FILE} ${EXECUTABLE_IHEX_FILE}
+    # Do this all in the build directory
     WORKING_DIRECTORY ${CMAKE_RUNTIME_OUTPUT_DIRECTORY}
 )
 
-# Add target for IHEX output so that it gets built
+# Add target for supplemental files so that they get built
 add_custom_target(
-    ${EXECUTABLE_NAME}-ihex
+    ${EXECUTABLE_NAME}-post-build
     ALL
-    DEPENDS ${EXECUTABLE_NAME}.ihex
+    DEPENDS
+        ${EXECUTABLE_DUMP_FILE}
+        ${EXECUTABLE_ELF_STRIPPED_FILE}
+        ${EXECUTABLE_IHEX_FILE}
 )
 
 # Clean additional support files
-set_directory_properties(
-    PROPERTIES
-    ADDITIONAL_MAKE_CLEAN_FILES ${PROJECT_BIN_DIR}/${EXECUTABLE_NAME}.ihex
+set_property(
+    DIRECTORY
+    APPEND PROPERTY
+    ADDITIONAL_MAKE_CLEAN_FILES
+        ${PROJECT_BIN_DIR}/${EXECUTABLE_DUMP_FILE}
+        ${PROJECT_BIN_DIR}/${EXECUTABLE_ELF_STRIPPED_FILE}
+        ${PROJECT_BIN_DIR}/${EXECUTABLE_IHEX_FILE}
+        ${PROJECT_BIN_DIR}/${EXECUTABLE_MAP_FILE}
 )
